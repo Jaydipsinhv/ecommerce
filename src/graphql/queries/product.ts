c84d520fe897bf8e042a67f2b6267448ffedb0c5
“Jaydipsinhv” <jaydip.vaghela@gmail.com>
2024-11-06 16:26:45 +0530

    tested and implemented first graphql query

--- src/graphql/queries/product.ts
+++ src/graphql/queries/product.ts
@@ -3,14 +3,16 @@ import { gql } from 'apollo-server-express';
 const product = {
   typeDefs: gql`
     extend type Query {
-      getProducts(id: ID!): Product!
+      getProducts(id: ID!): [Product]!
     }
   `,
 
   resolvers: {
-    async getProducts(_parent: any, args: { input: any; }, context: any) {
+    async getProducts(_parent: any, args: { id: any; }, context: any) {
+      const { id } = args;
+      console.log(`getProduct with id: ${id}`);
       return [{
-        id: '1',
+        id,
         name: 'product',
       }];
     },
